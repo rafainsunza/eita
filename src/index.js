27376c51a46cc71b0b92cf4ee3abe6ea0d80ccc2
rafainsunza <rafainsunza@outlook.com>
2025-02-03 12:34:20 +0100

    Restyle navbar

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import './javascript/custom-components/navbar/dt-navbar'
 import { fetchImage } from './javascript/utils'
 
 
-fetchImage('./assets/icons/eita_logo.png', document.querySelector('header').querySelector('.home-link'), 'logo')
+// fetchImage('./assets/icons/eita_logo.png', document.querySelector('header').querySelector('.home-link'), 'logo')
--- src/javascript/custom-components/navbar/dt-navbar.js
+++ src/javascript/custom-components/navbar/dt-navbar.js
@@ -19,21 +19,23 @@ class DtNavbar extends HTMLElement {
         this.shadowRoot.appendChild(template.content.cloneNode(true));
 
         this.nav = this.shadowRoot.querySelector('.nav');
+        this.navItems = this.shadowRoot.querySelector('.nav-items');
         this.openButton = this.shadowRoot.querySelector('.open-button');
         this.closeButton = this.shadowRoot.querySelector('.close-button');
 
         fetchImage('../../../assets/icons/eita_logo.png', this.nav.querySelector('.home-link'), 'logo')
+        fetchImage('../../../assets/icons/favicon.png', this.navItems.querySelector('.logo-button-container'), 'logo-small');
 
         this.openButton.addEventListener('click', () => { this.openNav() });
         this.closeButton.addEventListener('click', () => { this.closeNav() });
     }
 
     openNav() {
-        this.nav.classList.add('open');
+        this.navItems.classList.add('open');
     }
 
     closeNav() {
-        this.nav.classList.remove('open');
+        this.navItems.classList.remove('open');
     }
 
 }
